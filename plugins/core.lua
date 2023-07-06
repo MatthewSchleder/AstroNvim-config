50ae413402749b15646390f718fde4d2169cf79c
Matthew Schleder <matthew.schleder@sas.com>
2023-07-06 14:36:56 -0400

    Added flash and updated orgmode

--- plugins/core.lua
+++ plugins/core.lua
@@ -131,6 +131,60 @@ return {
     ft = { "markdown" }
   },
   {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+    keys = {
+      {
+        "s",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump({
+            search = {
+              mode = function(str)
+                return "\\<" .. str
+              end,
+            },
+          })
+        end,
+        desc = "Flash",
+      },
+      {
+        "S",
+        mode = { "n", "o", "x" },
+        function()
+          require("flash").treesitter()
+        end,
+        desc = "Flash Treesitter",
+      },
+      {
+        "r",
+        mode = "o",
+        function()
+          require("flash").remote()
+        end,
+        desc = "Remote Flash",
+      },
+      {
+        "R",
+        mode = { "o", "x" },
+        function()
+          require("flash").treesitter_search()
+        end,
+        desc = "Flash Treesitter Search",
+      },
+      {
+        "<c-s>",
+        mode = { "c" },
+        function()
+          require("flash").toggle()
+        end,
+        desc = "Toggle Flash Search",
+      },
+    },
+  },
+  {
     'nvim-orgmode/orgmode',
     -- dev = false,
     ft = 'org',
@@ -144,8 +198,7 @@ return {
       -- win_split_mode = 'auto',
 		  org_indent_mode = "noindent",
 		  org_capture_templates = {
-			  t = { description = "Todo Item", template = "* TODO %?\n%t\n  - "},
-			  -- p = { description = "Paste Todo Item", template = "* TODO %x%?\n  %t\n  - "},
+			  t = { description = "General Todo Item", template = "* TODO %?\n%t\n  - ", target = "~/org/agenda/todos.org"},
 		  },
 		  mappings = {
 			  org = {
